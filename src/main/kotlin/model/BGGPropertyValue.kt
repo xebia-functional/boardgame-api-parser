8d9e9253ff3a57cd62712ccddc6fc769785de522
Jesus Chaves <82162116+GachaDev@users.noreply.github.com>
2025-04-01 14:02:40 +0200

    Create BGGPropertyValue.kt

---
+++ src/main/kotlin/model/BGGPropertyValue.kt
@@ -0,0 +1,10 @@
+package com.es.model
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty
+
+data class BGGPropertyValue(
+    @JacksonXmlProperty(isAttribute = true)
+    @JsonProperty("value")
+    val value: String? = null
+)
