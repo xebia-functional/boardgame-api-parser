019656d68b61b1fa2f11c21b94ad24aed3c4e4c6
Jesus Chaves <82162116+GachaDev@users.noreply.github.com>
2025-04-01 12:59:04 +0200

    Create BGGGameItemName.kt

---
+++ src/main/kotlin/model/search/BGGGameItemName.kt
@@ -0,0 +1,14 @@
+package com.es.model.search
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty
+
+data class BGGGameItemName(
+    @JacksonXmlProperty(isAttribute = true)
+    @JsonProperty("type")
+    val type: String? = null,
+
+    @JacksonXmlProperty(isAttribute = true)
+    @JsonProperty("value")
+    val value: String? = null
+)
