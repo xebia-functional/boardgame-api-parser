dc3c2a1a784b0a13938413ac6df74194f6bc7753
Jesus Chaves <82162116+GachaDev@users.noreply.github.com>
2025-04-01 12:59:13 +0200

    Update SearchResponse.kt

--- src/main/kotlin/model/SearchResponse.kt
+++ src/main/kotlin/model/SearchResponse.kt
@@ -1,5 +1,6 @@
 package com.es.model
 
+import com.es.model.search.BGGGameItem
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper
