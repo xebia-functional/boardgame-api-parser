3ab4417adcba513aa8f535703fa89e14c1109df8
Jesus Chaves <82162116+GachaDev@users.noreply.github.com>
2025-04-01 14:02:32 +0200

    Create DetailResponse.kt

---
+++ src/main/kotlin/model/DetailResponse.kt
@@ -0,0 +1,15 @@
+package com.es.model
+
+import com.es.model.detail.BGGGameDetailItem
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class DetailResponse(
+    @JacksonXmlElementWrapper(useWrapping = false)
+    @JacksonXmlProperty(localName = "item")
+    @JsonProperty("items")
+    val items: List<BGGGameDetailItem> = emptyList(),
+)
