b43b11e6ed4f8dec8a64bc37cd7a1360c5ac826c
Gacha <82162116+GachaDev@users.noreply.github.com>
2025-04-08 11:39:22 +0200

    Revert "Create bgg controller"

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,6 +1,5 @@
 package com.es
 
-import com.es.controller.bgg
 import com.fasterxml.jackson.databind.*
 import io.ktor.serialization.jackson.*
 import io.ktor.server.application.*
@@ -9,15 +8,9 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
-    install(ContentNegotiation) {
-        jackson {
-            enable(SerializationFeature.INDENT_OUTPUT)
-        }
-    }
     routing {
         get("/") {
             call.respondText("Hello World!")
         }
-        bgg()
     }
 }
