86e79c9f1f1cda1db524af34fe428670cd6e816c
Jesus Chaves <82162116+GachaDev@users.noreply.github.com>
2025-04-09 13:12:23 +0200

    Update Routing.kt

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,9 +1,8 @@
 package com.es
 
 import com.es.controller.bgg
-import com.fasterxml.jackson.databind.*
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
-import io.ktor.serialization.jackson.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
@@ -11,10 +10,10 @@ import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     install(ContentNegotiation) {
-        jackson {
-            registerKotlinModule()
-            enable(SerializationFeature.INDENT_OUTPUT)
-        }
+        json(Json {
+            prettyPrint = true
+            ignoreUnknownKeys = true
+        })
     }
     routing {
         get("/") {
