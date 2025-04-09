29f3d91f8a8957e0b2814796df283496ddea6fab
Jesus Chaves <82162116+GachaDev@users.noreply.github.com>
2025-04-09 13:55:55 +0200

    Revert "Update BGGGameDetailItem.kt"
    This reverts commit da097b4fa6f2216752a09ee8588861fbd605ad71.

--- src/main/kotlin/model/detail/BGGGameDetailItem.kt
+++ src/main/kotlin/model/detail/BGGGameDetailItem.kt
@@ -2,50 +2,75 @@ package com.es.model.detail
 
 import com.es.model.search.BGGGameItemName
 import com.es.model.BGGPropertyValue
-import kotlinx.serialization.Serializable
-import nl.adaptivity.xmlutil.serialization.XmlElement
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty
 
-@Serializable
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class BGGGameDetailItem(
-    @XmlElement(true) // Atributo XML
+    @JacksonXmlProperty(isAttribute = true)
+    @JsonProperty("id")
     val id: String,
 
-    @XmlElement(true) // Atributo XML
+    @JacksonXmlProperty(isAttribute = true)
+    @JsonProperty("type")
     val type: String,
 
-    @XmlElement
+    @JacksonXmlProperty(localName = "thumbnail")
+    @JsonProperty("thumbnail")
     val thumbnail: String? = null,
 
-    @XmlElement
+    @JacksonXmlProperty(localName = "image")
+    @JsonProperty("image")
     val image: String? = null,
 
-    @XmlElement
-    val names: List<BGGGameItemName>? = null, // Lista de nombres
+    @JacksonXmlElementWrapper(useWrapping = false)
+    @JacksonXmlProperty(localName = "name")
+    @JsonProperty("names")
+    val names: List<BGGGameItemName>? = null,
 
-    @XmlElement
+    @JacksonXmlProperty(localName = "description")
+    @JsonProperty("description")
     val description: String? = null,
 
-    @XmlElement
+    @JacksonXmlElementWrapper(useWrapping = false)
+    @JacksonXmlProperty(localName = "yearpublished")
+    @JsonProperty("yearpublished")
     val yearPublished: BGGPropertyValue? = null,
 
-    @XmlElement
+    @JacksonXmlElementWrapper(useWrapping = false)
+    @JacksonXmlProperty(localName = "minplayers")
+    @JsonProperty("minplayers")
     val minPlayers: BGGPropertyValue? = null,
 
-    @XmlElement
+    @JacksonXmlElementWrapper(useWrapping = false)
+    @JacksonXmlProperty(localName = "maxplayers")
+    @JsonProperty("maxplayers")
     val maxPlayers: BGGPropertyValue? = null,
 
-    @XmlElement
+    @JacksonXmlElementWrapper(useWrapping = false)
+    @JacksonXmlProperty(localName = "playingtime")
+    @JsonProperty("playingtime")
     val playingTime: BGGPropertyValue? = null,
 
-    @XmlElement
+    @JacksonXmlElementWrapper(useWrapping = false)
+    @JacksonXmlProperty(localName = "minplaytime")
+    @JsonProperty("minplaytime")
     val minPlayTime: BGGPropertyValue? = null,
 
-    @XmlElement
+    @JacksonXmlElementWrapper(useWrapping = false)
+    @JacksonXmlProperty(localName = "maxplaytime")
+    @JsonProperty("maxplaytime")
     val maxPlayTime: BGGPropertyValue? = null,
 
-    @XmlElement
+    @JacksonXmlElementWrapper(useWrapping = false)
+    @JacksonXmlProperty(localName = "minage")
+    @JsonProperty("minage")
     val minAge: BGGPropertyValue? = null,
 
-    @XmlElement
-    val links: List<BGGGameDetailLink>? = null, // Lista de enlaces
+    @JacksonXmlElementWrapper(useWrapping = false)
+    @JacksonXmlProperty(localName = "link")
+    @JsonProperty("links")
+    val links: List<BGGGameDetailLink>? = null,
 )
