df32c761e7f5322e8df401693e75a30d378618a5
Jesus Chaves <82162116+GachaDev@users.noreply.github.com>
2025-04-09 13:12:32 +0200

    Update DetailResponse.kt

--- src/main/kotlin/model/DetailResponse.kt
+++ src/main/kotlin/model/DetailResponse.kt
@@ -1,15 +1,11 @@
 package com.es.model
 
 import com.es.model.detail.BGGGameDetailItem
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty
+import nl.adaptivity.xmlutil.serialization.XmlElement
+import kotlinx.serialization.Serializable
 
-@JsonIgnoreProperties(ignoreUnknown = true)
+@Serializable
 data class DetailResponse(
-    @JacksonXmlElementWrapper(useWrapping = false)
-    @JacksonXmlProperty(localName = "item")
-    @JsonProperty("items")
-    val items: List<BGGGameDetailItem>? = emptyList(),
+    @XmlElement(true) // Para manejar el wrapper XML
+    val items: List<BGGGameDetailItem> = emptyList()
 )
