74a011d3c351ecf08986ae6a641dfff6616cf1e3
Jesus Chaves <82162116+GachaDev@users.noreply.github.com>
2025-04-09 13:55:42 +0200

    Revert "Update Routing.kt"
    This reverts commit 86e79c9f1f1cda1db524af34fe428670cd6e816c.

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,8 +1,9 @@
 package com.es
 
 import com.es.controller.bgg
-import io.ktor.serialization.kotlinx.json.*
-import kotlinx.serialization.json.Json
+import com.fasterxml.jackson.databind.*
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import io.ktor.serialization.jackson.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
@@ -10,10 +11,10 @@ import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     install(ContentNegotiation) {
-        json(Json {
-            prettyPrint = true
-            ignoreUnknownKeys = true
-        })
+        jackson {
+            registerKotlinModule()
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
     }
     routing {
         get("/") {
