6c65428b0704e2b62a9e08fd362e831d5fd68799
Jesus Chaves <82162116+GachaDev@users.noreply.github.com>
2025-04-10 09:02:20 +0200

    Update BGGGameDetailLink.kt

--- src/main/kotlin/model/detail/BGGGameDetailLink.kt
+++ src/main/kotlin/model/detail/BGGGameDetailLink.kt
@@ -9,12 +9,10 @@ data class BGGGameDetailLink(
     @JacksonXmlProperty(isAttribute = true)
     @JsonProperty("type")
     val type: String? = null,
-
     @JacksonXmlProperty(isAttribute = true)
     @JsonProperty("id")
     val id: String? = null,
-
     @JacksonXmlProperty(isAttribute = true)
     @JsonProperty("value")
-    val value: String? = null
+    val value: String? = null,
 )
