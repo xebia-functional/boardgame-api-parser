b038e1c7c7034b867effb14c92bb45ab8cd3643f
Jesus Chaves <82162116+GachaDev@users.noreply.github.com>
2025-04-10 09:02:27 +0200

    Update BGGGameItem.kt

--- src/main/kotlin/model/search/BGGGameItem.kt
+++ src/main/kotlin/model/search/BGGGameItem.kt
@@ -3,22 +3,20 @@ package com.es.model.search
 import com.es.model.BGGPropertyValue
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class BGGGameItem(
     @JacksonXmlProperty(isAttribute = true)
     @JsonProperty("id")
     val id: String,
-
     @JacksonXmlElementWrapper(useWrapping = false)
     @JacksonXmlProperty(localName = "name")
     @JsonProperty("name")
     val name: BGGGameItemName? = null,
-
     @JacksonXmlElementWrapper(useWrapping = false)
     @JacksonXmlProperty(localName = "yearpublished")
     @JsonProperty("yearpublished")
-    val yearPublished: BGGPropertyValue? = null
+    val yearPublished: BGGPropertyValue? = null,
 )
